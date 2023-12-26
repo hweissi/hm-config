3ceebb99b985ae441944ac5984bca3cfaf1e3a03
Hannes Weissteiner <h.weissteiner@gmail.com>
2023-12-26 19:21:25 +0100

    temporarily disabled swayidle in homemanager, also kitty config (without package)

--- home.nix
+++ home.nix
@@ -62,292 +62,302 @@ rec {
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
+    ".config/scripts/lock.sh" = {
+      enable = false;
+      source = config/scripts/lock.sh;
+      executable = true;
+    };
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/hweissi/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     # EDITOR = "emacs";
     WORKON_HOME = "~/.virtualenvs";
   };
+
+  services = {
+    swayidle = {
+      # Can not enable before converting swayNC and swaylock
+      enable = false;
+      events = [
+        {
+          event = "before-sleep";
+          command = "loginctl lock-session";
+        }
+        {
+          event = "lock";
+          command = "/home/${home.username}/.config/scripts/lock.sh";
+        }
+      ];
+      systemdTarget = "graphical.target";
+    };
+  };
   
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-  # programs.zsh.ohMyZsh.customPkgs = [ pkgs.zsh-powerlevel10k ];
-#   programs.zsh = {
-#     enable = true;
-#     oh-my-zsh = {
-#       enable = true;
-#       plugins = [ 
-#           "git"
-#           "common-aliases"
-#           "thefuck"
-#           "zsh-syntax-highlighting"
-#           "zsh-autosuggestions"
-#           "bgnotify"
-#           "archlinux"
-#           "z"
-#           "cp"
-#           "transfer"];
-      
-#       #promptInit = "source ./config/zsh/p10k-config/.p10k.zsh";
-#       theme = "powerlevel10k";
-#       #setOptions = [
-#       #  "COMPLETION_WAITING_DOTS"
-#       #  "HIST_IGNORE_DUPS"
-#       #  "HIST_FCNTL_LOCK"
-#       #];
-#       shellAliases = {
-#         ls = "lsd --group-directories-first";
-#         pc = "echo -n $(pwd) | wl-copy --primary";
-#         icat = "kitty +kitten icat";
-#       };
-#       shellInit = ''function angrshell() {
-#     if [ $# -gt 1 ]
-#     then
-#         echo "Usage: angrshell <path>"
-#         return
-#     fi
-#     readonly vpath=''${1:-"."}
-#     docker run --rm -it -v "$vpath:/pwd" angr/angr
-# }
-# function gi() { curl -sLw n https://www.toptal.com/developers/gitignore/api/$@ ;}'';
-#     };
-#   };
 
+  programs = {
+    home-manager.enable = true;
 
-  programs.zsh = {
-    enable = true;
-    syntaxHighlighting.enable = true;
-    enableAutosuggestions = true;
-    oh-my-zsh = {
+    zsh = {
       enable = true;
-        plugins = [ 
-          "git"
-          "common-aliases"
-          "thefuck"
-          "bgnotify"
-          "archlinux"
-          "z"
-          "cp"
-          "transfer"
-        ];
-        extraConfig = 
-        ''
-        setopt appendhistory
-        '';
-    };
-    history = {
-      share = false;
+      syntaxHighlighting.enable = true;
+      enableAutosuggestions = true;
+      oh-my-zsh = {
+        enable = true;
+          plugins = [ 
+            "git"
+            "common-aliases"
+            "thefuck"
+            "bgnotify"
+            "archlinux"
+            "z"
+            "cp"
+            "transfer"
+          ];
+          extraConfig = 
+          ''
+          setopt appendhistory
+          '';
+      };
+      history = {
+        share = false;
 
-    };
-    sessionVariables = {
-      COMPLETION_WAITING_DOTS = true;
-    };
-    shellAliases = {
-      ls = "lsd --group-directories-first";
-      pc = "echo -n $(pwd) | wl-copy --primary";
-      icat = "kitty +kitten icat";
-    };
-    initExtraFirst = 
-    ''
-    source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-    if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-      source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-    fi
-    '';
-    initExtra = 
-    ''
-    function angrshell() {
-      if [ $# -gt 1 ]
-      then
-          echo "Usage: angrshell <path>"
-          return
+      };
+      sessionVariables = {
+        COMPLETION_WAITING_DOTS = true;
+      };
+      shellAliases = {
+        ls = "lsd --group-directories-first";
+        pc = "echo -n $(pwd) | wl-copy --primary";
+        icat = "kitty +kitten icat";
+      };
+      initExtraFirst = 
+      ''
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
       fi
-      readonly vpath=''${1:-"."}
-      docker run --rm -it -v "$vpath:/pwd" angr/angr
-    }
-    function gi() { curl -sLw n https://www.toptal.com/developers/gitignore/api/$@ ;}
+      '';
+      initExtra = 
+      ''
+      function angrshell() {
+        if [ $# -gt 1 ]
+        then
+            echo "Usage: angrshell <path>"
+            return
+        fi
+        readonly vpath=''${1:-"."}
+        docker run --rm -it -v "$vpath:/pwd" angr/angr
+      }
+      function gi() { curl -sLw n https://www.toptal.com/developers/gitignore/api/$@ ;}
 
-    source /usr/bin/virtualenvwrapper.sh
+      source /usr/bin/virtualenvwrapper.sh
 
-    source ~/.p10k.zsh
-    '';
-  };
-  programs.git = {
-    enable = true;
-    userEmail = "h.weissteiner@gmail.com";
-    userName = "Hannes Weissteiner";
-    extraConfig = {
-      pull.rebase = false;
+      source ~/.p10k.zsh
+      '';
     };
-  };
+    git = {
+      enable = true;
+      userEmail = "h.weissteiner@gmail.com";
+      userName = "Hannes Weissteiner";
+      extraConfig = {
+        pull.rebase = false;
+      };
+    };
 
-  programs.vscode = {
-    enable = true;  
-    extensions = 
-    let 
-      vscode-market =
-        (import (builtins.fetchGit {
-          url = "https://github.com/nix-community/nix-vscode-extensions";
-          ref = "refs/heads/master";
-          rev = "c43d9089df96cf8aca157762ed0e2ddca9fcd71e";
-      })).extensions.${pkgs.system}.vscode-marketplace;
-    in 
-      [
-        vscode-market.endormi."2077-theme"
-        vscode-market.znck.grammarly
-        vscode-market.atommaterial.a-file-icon-vscode
-        vscode-market.donjayamanne.python-environment-manager
-        pkgs.vscode-extensions.ziglang.vscode-zig
-        pkgs.vscode-extensions.ms-vscode.cpptools
-        pkgs.vscode-extensions.tamasfe.even-better-toml
-        pkgs.vscode-extensions.james-yu.latex-workshop
-        pkgs.vscode-extensions.marp-team.marp-vscode
-        pkgs.vscode-extensions.jnoortheen.nix-ide
-        pkgs.vscode-extensions.ms-python.vscode-pylance
-        pkgs.vscode-extensions.ms-vscode-remote.remote-ssh
-      ];
-    enableExtensionUpdateCheck = false;
-    enableUpdateCheck = false;
-    mutableExtensionsDir = false;
-    userSettings = {
-        "workbench.colorTheme" = "2077";
-        "workbench.iconTheme" = "a-file-icon-vscode";
-        "python.experiments.enabled" = false;
-        "python.globalModuleInstallation" = true;
-        "editor.inlayHints.enabled" = "offUnlessPressed";
-        "editor.fontFamily" = "'FiraCode Nerd Font'";
-        "editor.fontLigatures" = true;
-        "window.titleBarStyle" = "custom";
-        "workbench.editorAssociations" = {
-            "*.bin" = "default";
-            "*.pdf" = "latex-workshop-pdf-hook";
-        };
-        "extensions.autoUpdate" = false;
-        "latex-workshop.latex.recipes"= [
-
-
-            {
-                "name" = "latexmk";
-                "tools" = [
-                    "latexmk"
-                ];
-            }
-            {
-                "name" = "latexmk (latexmkrc)";
-                "tools" = [
-                    "latexmk_rconly"
-                ];
-            }
-            {
-                "name" = "latexmk (lualatex)";
-                "tools" = [
-                    "lualatexmk"
-                ];
-            }
-            {
-                "name" = "latexmk (xelatex)";
-                "tools" = [
-                    "xelatexmk"
-                ];
-            }
-            {
-                "name" = "pdflatex -> bibtex -> pdflatex * 2";
-                "tools" = [
-                    "pdflatex"
-                    "bibtex"
-                    "pdflatex"
-                    "pdflatex"
-                ];
-            }
-            {
-                "name" = "Compile Rnw files";
-                "tools" = [
-                    "rnw2tex"
-                    "latexmk"
-                ];
-            }
-            {
-                "name" = "Compile Jnw files";
-                "tools" = [
-                    "jnw2tex"
-                    "latexmk"
-                ];
-            }
-            {
-                "name" = "Compile Pnw files";
-                "tools" = [
-                    "pnw2tex"
-                    "latexmk"
-                ];
-            }
-            {
-                "name" = "tectonic";
-                "tools" = [
-                    "tectonic"
-                ];
-            }
+    vscode = {
+      enable = true;  
+      extensions = 
+      let 
+        vscode-market =
+          (import (builtins.fetchGit {
+            url = "https://github.com/nix-community/nix-vscode-extensions";
+            ref = "refs/heads/master";
+            rev = "c43d9089df96cf8aca157762ed0e2ddca9fcd71e";
+        })).extensions.${pkgs.system}.vscode-marketplace;
+      in 
+        [
+          vscode-market.endormi."2077-theme"
+          vscode-market.znck.grammarly
+          vscode-market.atommaterial.a-file-icon-vscode
+          vscode-market.donjayamanne.python-environment-manager
+          pkgs.vscode-extensions.ziglang.vscode-zig
+          pkgs.vscode-extensions.ms-vscode.cpptools
+          pkgs.vscode-extensions.tamasfe.even-better-toml
+          pkgs.vscode-extensions.james-yu.latex-workshop
+          pkgs.vscode-extensions.marp-team.marp-vscode
+          pkgs.vscode-extensions.jnoortheen.nix-ide
+          pkgs.vscode-extensions.ms-python.vscode-pylance
+          pkgs.vscode-extensions.ms-vscode-remote.remote-ssh
         ];
-        "latex-workshop.latex.external.build.command" = "make";
-        "[latex]" = {
-            "editor.wordWrap" = "bounded";
-            "editor.wordWrapColumn" = 120;
-        };
-        "grammarly.config.documentDomain" = "academic";
-        "grammarly.config.suggestionCategories.ConjunctionAtStartOfSentence" = true;
-        "grammarly.config.suggestionCategories.InformalPronounsAcademic" = true;
-        "grammarly.config.suggestionCategories.OxfordComma" = true;
-        "python.defaultInterpreterPath" = "/home/${home.username}/.virtualenvs/ctf";
-        "window.zoomLevel" = 2;
-        "nix.enableLanguageServer" = true;
-        "nix.serverPath" = "nixd";
-        "nix.serverSettings" = {
-          "nixd" = {
-            "eval" = {
-              "target" = {
-                # Accept args as "nix eval"
-                "args" = [];
-                # "nix eval"
-                "installable" = "";
+      enableExtensionUpdateCheck = false;
+      enableUpdateCheck = false;
+      mutableExtensionsDir = false;
+      userSettings = {
+          "workbench.colorTheme" = "2077";
+          "workbench.iconTheme" = "a-file-icon-vscode";
+          "python.experiments.enabled" = false;
+          "python.globalModuleInstallation" = true;
+          "editor.inlayHints.enabled" = "offUnlessPressed";
+          "editor.fontFamily" = "'FiraCode Nerd Font'";
+          "editor.fontLigatures" = true;
+          "window.titleBarStyle" = "custom";
+          "workbench.editorAssociations" = {
+              "*.bin" = "default";
+              "*.pdf" = "latex-workshop-pdf-hook";
+          };
+          "extensions.autoUpdate" = false;
+          "latex-workshop.latex.recipes"= [
+
+
+              {
+                  "name" = "latexmk";
+                  "tools" = [
+                      "latexmk"
+                  ];
+              }
+              {
+                  "name" = "latexmk (latexmkrc)";
+                  "tools" = [
+                      "latexmk_rconly"
+                  ];
+              }
+              {
+                  "name" = "latexmk (lualatex)";
+                  "tools" = [
+                      "lualatexmk"
+                  ];
+              }
+              {
+                  "name" = "latexmk (xelatex)";
+                  "tools" = [
+                      "xelatexmk"
+                  ];
+              }
+              {
+                  "name" = "pdflatex -> bibtex -> pdflatex * 2";
+                  "tools" = [
+                      "pdflatex"
+                      "bibtex"
+                      "pdflatex"
+                      "pdflatex"
+                  ];
+              }
+              {
+                  "name" = "Compile Rnw files";
+                  "tools" = [
+                      "rnw2tex"
+                      "latexmk"
+                  ];
+              }
+              {
+                  "name" = "Compile Jnw files";
+                  "tools" = [
+                      "jnw2tex"
+                      "latexmk"
+                  ];
+              }
+              {
+                  "name" = "Compile Pnw files";
+                  "tools" = [
+                      "pnw2tex"
+                      "latexmk"
+                  ];
+              }
+              {
+                  "name" = "tectonic";
+                  "tools" = [
+                      "tectonic"
+                  ];
+              }
+          ];
+          "latex-workshop.latex.external.build.command" = "make";
+          "[latex]" = {
+              "editor.wordWrap" = "bounded";
+              "editor.wordWrapColumn" = 120;
+          };
+          "grammarly.config.documentDomain" = "academic";
+          "grammarly.config.suggestionCategories.ConjunctionAtStartOfSentence" = true;
+          "grammarly.config.suggestionCategories.InformalPronounsAcademic" = true;
+          "grammarly.config.suggestionCategories.OxfordComma" = true;
+          "python.defaultInterpreterPath" = "/home/${home.username}/.virtualenvs/ctf";
+          "window.zoomLevel" = 2;
+          "nix.enableLanguageServer" = true;
+          "nix.serverPath" = "nixd";
+          "nix.serverSettings" = {
+            "nixd" = {
+              "eval" = {
+                "target" = {
+                  # Accept args as "nix eval"
+                  "args" = [];
+                  # "nix eval"
+                  "installable" = "";
+                };
+                # Extra depth for evaluation
+                "depth" = 0;
+                # The number of workers for evaluation task.
+                "workers" = 3;
               };
-              # Extra depth for evaluation
-              "depth" = 0;
-              # The number of workers for evaluation task.
-              "workers" = 3;
-            };
-            #"formatting" = {
-              # Which command you would like to do formatting
-              # "command" = "nixpkgs-fmt";
-            #};
-            "options" = {
-              # Disable it if you are not writting modules.
-              "enable" = true;
-              "target" = {
-                "args" = [];
-                # Example of NixOS options.
-                "installable" = "<flakeref>#homeConfigurations.<name>.options";
+              #"formatting" = {
+                # Which command you would like to do formatting
+                # "command" = "nixpkgs-fmt";
+              #};
+              "options" = {
+                # Disable it if you are not writting modules.
+                "enable" = true;
+                "target" = {
+                  "args" = [];
+                  # Example of NixOS options.
+                  "installable" = "<flakeref>#homeConfigurations.<name>.options";
+                };
               };
             };
           };
-        };
 
+      };
+      
     };
+
+    kitty = {
+      enable = true;
+      font = {
+        name = "JetBrains Mono Bold";
+        package = pkgs.jetbrains-mono;
+        size = 12.0;
+      };
+      package = pkgs.hello; # just install random trash until I fix nixGL... use system kitty in the meantime
+
+
+      extraConfig = ''
+        text_composition_strategy legacy
+        cursor_shape block
+        cursor_blink_interval 0
+        scrollback_lines 5000
+        scrollback_fill_enlarged_window yes
+        wheel_scroll_multiplier 5.0
+        touch_scroll_multiplier 5.0
+        url_color #5555cc
+        url_style double
+        show_hyperlink_targets yes
+        paste_actions quote-urls-at-prompt,confirm
+        window_alert_on_bell yes
+        bell_on_tab "🔔 "
+        background_opacity 0.3
+        background_blur 1
+        dynamic_background_opacity yes
+        selection_foreground none
+        selection_background none
+
+      '';
+      shellIntegration = {
+        enableZshIntegration = true;
+        mode = "no-cursor";
+      };
+      theme = "Mathias";
+    };
+
     
   };
+
   manual = {
     json.enable = true;
     html.enable = true;
